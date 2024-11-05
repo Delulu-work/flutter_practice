5ce1d8e00d888c82ceaf64b0589ab9bf65f5871c
Delulu-work <sanchezyessica199@gmail.com>
2024-11-04 23:10:16 -0400

    Mi primer commit

--- index.dart
+++ index.dart
@@ -1,1 +1,28 @@
-hola mundo
+void main() {
+    List<int> listaNumeros = [1, 2, 3, 4, 5];
+    print(listaNumeros);
+    print(listaNumeros[3]);
+
+    listaNumeros[4] = 8;
+    print(listaNumeros);
+    
+    var lenguajesPro = {
+        //key : name
+        'py': 'python',
+        'dart': 'dart',
+        'js' : 'java script'
+    };
+
+    print(lenguajesPro['js']);
+
+    var nuevosLenguajes = {'React' : 'React'
+    };
+
+    lenguajesPro.addAll(nuevosLenguajes);
+
+    print(lenguajesPro);
+
+    
+
+    
+}
